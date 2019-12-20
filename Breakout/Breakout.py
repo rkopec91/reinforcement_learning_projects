2834eb2cefa9c3b7a498c790f409538b84c0ce25
rkopec91 <ryankopec91@gmail.com>
2019-12-19 23:18:34 -0500

    Breakout updates

--- Breakout/Breakout.py
+++ Breakout/Breakout.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import numpy as np
 from skimage.transform import resize
 import torchvision as tv
+from ImageProcessor import ImageProcessor
 
 
 class Game:
@@ -23,6 +24,7 @@ class Game:
         self.lives = 0
         self.steps = steps
         self.history_length = history_length
+        self.process_frame = ImageProcessor()
 
     def reset_game(self, evaluation=False):
         frame = self.env.reset()
@@ -33,23 +35,23 @@ class Game:
             for _ in range(random.randint(1,self.steps)):
                 frame, _, _, _ = self.env.step(1)
 
-        processed_frame = self.process_frame(frame)   # (★★★)
+        processed_frame = self.process_frame(frame)
         self.state = np.repeat(processed_frame, self.agent_history_length, axis=2)
-        
+
         return terminal_life
 
     def step(self, action):
 
-        new_frame, reward, terminal, info = self.env.step(action)  # (5★)
-            
+        new_frame, reward, terminal, info = self.env.step(action)
+
         if info['ale.lives'] < self.last_lives:
             terminal_life_lost = True
         else:
             terminal_life_lost = terminal
         self.last_lives = info['ale.lives']
-        
-        processed_new_frame = self.process_frame(sess, new_frame)   # (6★)
-        new_state = np.append(self.state[:, :, 1:], processed_new_frame, axis=2) # (6★)   
+
+        processed_new_frame = self.process_frame(new_frame)
+        new_state = np.append(self.state[:, :, 1:], processed_new_frame, axis=2)
         self.state = new_state
-        
+
         return processed_new_frame, reward, terminal, terminal_life_lost, new_frame
--- Breakout/run.py
+++ Breakout/run.py
@@ -1,8 +1,11 @@
 from Breakout import Game
-import DQN
+from DQN import DQN
 import argparse
 
 def run(model):
+    pass
+
+def learn():
     pass
 
 def train(model_name):
@@ -23,6 +26,8 @@ if __name__ == "__main__":
 
     save_path = "./"
     environment = "BreakoutDeterministic-v4"
+    
+    dqn = DQN(4)
 
     game = Game()
 
